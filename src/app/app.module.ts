9286fba3141779a91b84db5009b65ceac20b75b9
shivaji33 <naidupilla84@gmail.com>
2020-11-20 11:04:00 +0530

    2 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 import { GlobalErrorService } from './global-error.service';
+import { ErrorService } from './error.service';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ],
   providers: [
+    ErrorService,
      { provide: ErrorHandler, useClass: GlobalErrorService }
   ],
    schemas: [CUSTOM_ELEMENTS_SCHEMA]
